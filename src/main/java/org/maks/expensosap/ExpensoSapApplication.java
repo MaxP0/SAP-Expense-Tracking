1d333bc368d6d943b2e91213bc33a3199b7b53a6
Maks <maxf25240@gmail.com>
2025-12-06 16:06:30 +0000

    initial commit

---
+++ src/main/java/org/maks/expensosap/ExpensoSapApplication.java
@@ -0,0 +1,13 @@
+package org.maks.expensosap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensoSapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpensoSapApplication.class, args);
+    }
+
+}
